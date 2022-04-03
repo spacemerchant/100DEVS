09c720649aead4ebb3561de658a31d809ffe34c1
Christopher Lopez <chris.lopez174@gmail.com>
2022-04-03 12:06:13 -0400

    Adding Mosh OOP HW File

---
+++ HW#11/Mosh-OOP.js
@@ -0,0 +1,13 @@
+function stopWatch(){
+    return {
+        
+        
+    }
+}
+
+
+// duration function that tells current duration
+// start function with error to stop multiple stops
+// stop funtion with error to stop multiple
+// reset function
+
